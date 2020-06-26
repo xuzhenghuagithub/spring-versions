fbd75ed87a301ff40a1f1f7fdea1a79002416684
xuzhenghua <291493459@qq.com>
2020-06-26 19:17:50 +0800

    import

--- src/main/java/com/cnpc/demo/spring3x/Spring3xConfiguration.java
+++ src/main/java/com/cnpc/demo/spring3x/Spring3xConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
  * spring3.x  纯注解去xml
  * configuration注解取代applicationContext.xml文件
  * ComponentScan注解取代<context:component-scan base-package="com.cnpc.demo.spring3x"/>
- * import注解取代<import resource="classpath:applicationContext1.xml"/>
+ * import注解取代<import resource="classpath:applicationContext1.xml"/>,导入配置类或者Bean
  */
 @Import(Spring3xImportConfiguration.class)
 @ComponentScan("com.cnpc.demo.spring3x")
